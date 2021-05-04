b872887c696c7a73b8bbad7b2202da4e47905e10
Anjelica <ambrosio.anjelica@gmail.com>
2021-05-03 18:57:25 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+/// reikamoon's library
+
+function removeVowels(str) {
+    var strWithoutVowels =  [];
+            str = str.split('');
+            var vowels = ['a', 'e', 'i', 'o', 'u'];
+            for (var i = 0; i < str.length; i++) {
+                if (vowels.indexOf(str[i]) < 0) {
+                    strWithoutVowels.push(str[i])
+                }
+            }
+            strWithoutVowels = strWithoutVowels.join('');
+            return strWithoutVowels;
+        }
+
+String.prototype.removeVowels = function () {
+  return removevowels(this);
+};
+
+console.log('1 - No Vowels:  ' + removeVowels("I've been waiting for this!"))
+
+
+function mirror(str) {
+  const split = str.split("")
+  const reverseArray = split.reverse();
+  const joinArray = reverseArray.join("");
+  return joinArray 
+}
+
+String.prototype.mirror = function () {
+    return mirror(this);
+};
+
+console.log('2 - Mirror Words: ' + mirror('hello hello hello'))
+
+function babyTalk(str) {
+    const lower = str.toLowerCase()
+    const search = "l"
+    const baby = "w"
+    const result = lower.replaceAll(search, baby)
+    const no = "no"
+    const noes = "noes"
+    const noesie = result.replaceAll(no, noes)
+    const r = "r"
+    const babyr = "w"
+    const babiedr = noesie.replaceAll(r, babyr)
+    const mistake = "mistake"
+    const oopsie = "oopsie-whoopsie"
+    const replacemistake = babiedr.replaceAll(mistake, oopsie)
+    const error = "bugs"
+    const oopsie2 = "bugsie-wugsie"
+    const replaceError = replacemistake.replaceAll(error, oopsie2)
+    const owo = replaceError + " " + "owo"
+    return owo
+}
+
+String.prototype.babyTalk = function () {
+  return babyTalk(this);
+};
+
+console.log('3 - Baby Talk: ' + babyTalk("This library was a mistake."))
+
+
+module.exports = {
+  removeVowels,
+  mirror,
+  babyTalk,
+}
---
+++ tests/library.test.js
@@ -0,0 +1,16 @@
+const rm = require('../src/index')
+
+test('removeVowels', () => {
+  expect(rm.removeVowels("apple")).toBe("ppl");
+  expect(rm.removeVowels("orange")).toBe("rng");
+});
+
+test('mirror', () => {
+    expect(rm.mirror('hello')).toBe("olleh");
+    expect(rm.mirror('rorrim')).toBe('mirror');
+})
+
+test('babyTalk', () => {
+    expect(rm.babyTalk('Bugs and mistake')).toBe("bugsie-wugsie and oopsie-whoopsie owo")
+    expect(rm.babyTalk('Hello, how are you?')).toBe("hewwo, how awe you? owo")
+})
